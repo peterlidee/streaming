fd9ff4173d7758eafd47c07564318edfbb913c9e
peterlidee <peter@lidee.be>
2023-09-10 15:54:06 +0200

    Added test 3

---
+++ app/test3/[pageId]/page.tsx
@@ -0,0 +1,63 @@
+import { Suspense } from 'react';
+import Loader from '@/components/Loader';
+import Post from '@/components/Post';
+
+type Props = {
+  params: {
+    pageId: string;
+  };
+};
+
+export default function Page({ params }: Props) {
+  return (
+    <div>
+      <ol>
+        <Suspense
+          fallback={
+            <li>
+              <Loader />
+            </li>
+          }
+        >
+          <Post delay={0} />
+        </Suspense>
+        <Suspense
+          fallback={
+            <li>
+              <Loader />
+            </li>
+          }
+        >
+          <Post delay={300} />
+        </Suspense>
+        <Suspense
+          fallback={
+            <li>
+              <Loader />
+            </li>
+          }
+        >
+          <Post delay={600} />
+        </Suspense>
+        <Suspense
+          fallback={
+            <li>
+              <Loader />
+            </li>
+          }
+        >
+          <Post delay={900} />
+        </Suspense>
+        <Suspense
+          fallback={
+            <li>
+              <Loader />
+            </li>
+          }
+        >
+          <Post delay={1200} />
+        </Suspense>
+      </ol>
+    </div>
+  );
+}
---
+++ app/test3/layout.tsx
@@ -0,0 +1,23 @@
+import Nav from '@/components/Nav';
+import Link from 'next/link';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function layout({ children }: Props) {
+  const path = '/test3';
+  return (
+    <div>
+      <h1>Test 3</h1>
+      <Nav>
+        <Link href={`${path}/1`}>page 1</Link>
+        <Link href={`${path}/2`}>page 2</Link>
+        <Link href={`${path}/3`}>page 3</Link>
+        <Link href={`${path}/4`}>page 4</Link>
+        <Link href={`${path}/5`}>page 5</Link>
+      </Nav>
+      {children}
+    </div>
+  );
+}
