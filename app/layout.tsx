206eb5d740c6d271f956e43ad3a9f6359ab3bbbf
peterlidee <peter@lidee.be>
2023-09-10 15:53:13 +0200

    Updated routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
+import Nav from '@/components/Nav';
 import type { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Routing tests',
@@ -11,7 +13,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <Nav>
+          <Link href='/'>home</Link>
+          <Link href='/test1'>test 1</Link>
+          <Link href='/test2'>test 2</Link>
+          <Link href='/test3'>test 3</Link>
+          <Link href='/test4'>test 4</Link>
+        </Nav>
+        {children}
+      </body>
     </html>
   );
 }
